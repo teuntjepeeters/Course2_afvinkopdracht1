2a537cfda74ba53abff328f25820992617955065
lussj <teuntje.peeters@han.nl>
2021-10-15 09:55:38 +0200

    Hamming distance

---
+++ hamming_distance.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    training_bestand = "hamming_distance_training.fa"
+    test_bestand = "hamming_distance_test.fa"
+
+    # Lees de twee sequenties in
+
+    # Bereken de hamming distance van deze twee sequenties door ze te
+    # vergelijken per positie en de verschillen bij elkaar op te tellen
+
+
+main()
